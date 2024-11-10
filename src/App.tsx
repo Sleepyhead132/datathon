5ad9dd916d2629b71636835b4905dee15f659917
abhinav-akavaram <121636314+abhinav-akavaram@users.noreply.github.com>
2024-11-09 20:33:16 -0600

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import CustomerBehaviorDashboard from './CustomerBehaviorDashboard';
+
+function App() {
+  return (
+    <div>
+      <CustomerBehaviorDashboard />
+    </div>
+  );
+}
+
+export default App;
